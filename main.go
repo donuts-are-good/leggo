c0d2023cd2ef3152391fa3d4e107483e50481b88
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-04-07 22:50:26 -0500

    refactor
    moved devname to a global var, along with project path.
    
    also added more boilerplate that im typing in anyway, like the program name in the readme and the license text

--- main.go
+++ main.go
@@ -9,61 +9,12 @@ import (
 	"strings"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Program name: ")
-	name := scanInput(scanner)
-	dir := filepath.Join(os.Getenv("HOME"), "Projects", name)
-	fmt.Print("Library or program (l/p)? ")
-	lp := scanInput(scanner)
-	os.MkdirAll(dir, 0755)
-	assets(scanner, dir)
-	summary(name, dir, lp)
-	if strings.ToLower(scanInput(scanner)) != "y" {
-		return
-	}
-	setup(name, dir, lp)
-}
-
-func scanInput(s *bufio.Scanner) string { s.Scan(); return s.Text() }
-
-func assets(s *bufio.Scanner, d string) {
-	fmt.Print("Assets folder? (Y/N) ")
-	if strings.ToLower(scanInput(s)) == "y" {
-		os.MkdirAll(d+"-assets", 0755)
-	}
-}
-
-func setup(n, d, lp string) {
-	if lp == "l" {
-		createFile(filepath.Join(d, n+".go"), "")
-	} else {
-		createFile(filepath.Join(d, "main.go"), `package main; func main() { println("alive!")}`)
-	}
-	createFile(filepath.Join(d, "README.md"), "")
-	if lp == "p" {
-		createLicense(d)
-	}
-	execCmd("go", "mod", "init", "github.com/donuts-are-good/"+n, d)
-	execCmd("go", "mod", "tidy", d)
-	execCmd("git", "init", d)
-	execCmd("git", "remote", "add", "origin", "https://github.com/donuts-are-good/"+n+".git", d)
-	execCmd("git", "add", "-A", ".", ".gitignore", d)
-	createFile(filepath.Join(d, ".gitignore"), fmt.Sprintf(".DS_Store\n.Trash-1000\n%s", n))
-	execCmd("code", ".", d)
-}
-
-func execCmd(name string, arg ...string) {
-	cmd := exec.Command(name, arg[1:]...)
-	cmd.Dir = arg[0]
-	cmd.Run()
-}
-
-func createFile(p, c string) { f, _ := os.Create(p); f.WriteString(c); f.Close() }
-
-func createLicense(d string) {
-	license := `
+var (
+	devname     = "donuts-are-good"
+	projectpath = filepath.Join(os.Getenv("HOME"), "Projects")
+	licensetext = `
 MIT License
+
 Copyright (c) 2023 donuts-are-good <https://github.com/donuts-are-good>
 	
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -83,7 +34,80 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.`
-	createFile(filepath.Join(d, "LICENSE.md"), license)
+)
+
+func main() {
+
+	// listen for input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// get the program name from the user
+	fmt.Print("Program name: ")
+
+	// the thing we're listening for is a program or lib name
+	name := scanInput(scanner)
+
+	// it's going in ~/Projects/... by deault
+
+	dir := filepath.Join(projectpath, name)
+
+	// see if it is a library or program
+	fmt.Print("Library or program (l/p)? ")
+	lp := scanInput(scanner)
+
+	// make the directory for the proejct
+	os.MkdirAll(dir, 0755)
+
+	// if there are assets desired, create an n+-assets folder
+	assets(scanner, dir)
+
+	// present a summary of the actions to the user
+	summary(name, dir, lp)
+	if strings.ToLower(scanInput(scanner)) != "y" {
+		return
+	}
+	setup(name, dir, lp)
+}
+
+func scanInput(s *bufio.Scanner) string { s.Scan(); return s.Text() }
+
+func assets(s *bufio.Scanner, d string) {
+	fmt.Print("Would you like a " + d + "-assets folder? (Y/N) ")
+	if strings.ToLower(scanInput(s)) == "y" {
+		os.MkdirAll(d+"-assets", 0755)
+	}
+}
+
+func setup(n, d, lp string) {
+	if lp == "l" {
+		createFile(filepath.Join(d, n+".go"), "package "+n)
+	} else {
+		createFile(filepath.Join(d, "main.go"), "package main;\n\n func main() { \n\tprintln(\"alive!\")\n}")
+	}
+	createFile(filepath.Join(d, "README.md"), "# "+n+"\n")
+	if lp == "p" {
+		createLicense(d)
+	}
+	execCmd("go", "mod", "init", "github.com/"+devname+"/"+n, d)
+	execCmd("go", "mod", "tidy", d)
+	execCmd("git", "init", d)
+	execCmd("git", "remote", "add", "origin", "https://github.com/"+devname+"/"+n+".git", d)
+	execCmd("git", "add", "-A", ".", ".gitignore", d)
+	createFile(filepath.Join(d, ".gitignore"), fmt.Sprintf(".DS_Store\n.Trash-1000\n%s", n))
+	execCmd("code", ".", d)
+}
+
+func execCmd(name string, arg ...string) {
+	cmd := exec.Command(name, arg[1:]...)
+	cmd.Dir = arg[0]
+	cmd.Run()
+}
+
+func createFile(p, c string) { f, _ := os.Create(p); f.WriteString(c); f.Close() }
+
+func createLicense(d string) {
+
+	createFile(filepath.Join(d, "LICENSE.md"), licensetext)
 }
 
 func summary(n, d, lp string) {
